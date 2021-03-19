0bfa927cf185bab25e2b26e1443a8bff1018e74f
KainanGabriel <40269057+KainanGabriel@users.noreply.github.com>
2021-03-19 09:52:32 -0300

    parte 5

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -3,7 +3,7 @@
         <div class="card">
             <div class="card-image">
                 <figure>
-                <img :src="pokemon.front" alt="Pokemon">
+                <img :src="currentImg" alt="Pokemon">
                 </figure>
             </div>
             <div class="card-content">
@@ -15,7 +15,7 @@
                 </div>
 
                 <div class="content">
-                
+                    <button class="button is-medium" @click="mudarSprite">Mudar Sprite</button>
                 </div>
             </div>
         </div>
@@ -32,11 +32,14 @@ export default {
             this.pokemon.type = res.data.types[0].type.name;
             this.pokemon.front = res.data.sprites.front_default;
             this.pokemon.back = res.data.sprites.back_default;
+            this.currentImg = this.pokemon.front;
             console.log(this.pokemon);
         })
     },
     data(){
       return{
+          isFront: true,
+          currentImg : '',
           pokemon:{
               type: '',
               front: '',
@@ -53,6 +56,17 @@ export default {
         upper: function(value){
             var newName = value[0].toUpperCase() + value.slice(1);
             return newName;
+        }
+    },
+    methods:{
+        mudarSprite: function(){
+            if(this.isFront){
+                this.isFront = false;
+                this.currentImg = this.pokemon.back;
+            }else{
+                this.isFront = true;
+                this.currentImg = this.pokemon.front;
+            }
         }
     }
 }
